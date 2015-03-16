7ed08cf2d59299f3e7815697f5ddfb90557deb5f
Javi Er <javier.caceres77@gmail.com>
2015-03-16 11:16:46 +0000

    added tmp

---
+++ tmp/sql/delete album.sql
@@ -0,0 +1,24 @@
+delete from images 
+where photo_id in (
+	select photo_id
+	from album_photo
+	where album_id = 13);
+	
+	
+delete from photos
+where photo_id in (
+	select photo_id
+	from album_photo
+	where album_id = 13);
+
+delete from album_photo 
+where album_id = 13;
+
+delete from album_location
+where album_id = 13;
+
+delete from users_albums
+where album_id = 13;
+
+delete from albums
+where album_id = 13;
